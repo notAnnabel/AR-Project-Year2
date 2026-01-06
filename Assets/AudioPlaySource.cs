fe4433e842c65dc4c5d8049805100995ff2b1762
annabeldornan <Annabel.Dornan@student.norwichuni.ac.uk>
2026-01-06 15:01:55 +0000

    save from uni

---
+++ Assets/AudioPlaySource.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class AudioPlaySource : MonoBehaviour
+{
+    public AudioSource audioSource;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void Start()
+    {
+        AudioSource.play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void playing(){
+        audioSource.play();
+    }
+}
